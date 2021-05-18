6d0f7687949f7852c28dc32b18645e51aae3003b
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-18 11:41:31 +0800

    Update 20150311002558_create_page_content.rb

--- db/migrate/20150311002558_create_page_content.rb
+++ db/migrate/20150311002558_create_page_content.rb
@@ -6,6 +6,10 @@ class CreatePageContent < ActiveRecord::Migration
     end
 
     # Create a single row for the about page content
-    PageContent.create!(:about_page_content => 'Test Content')
+    PageContent.create!(:about_page_content => 'OurFlora is an extension of the CampusFlora project. OurFlora allows institutions, organisations and community groups to offer vegetation maps to showcase the diversity of botanical life growing around them. Both OurFlora and CampusFlora are lead by A/Prof Rosanne Quinnell, School of Life and Environmental Sciences, The University of Sydney.
+
+    IT development of OurFlora was undertaken by Faculty of Engineering students (Jingqi Liu, Xiaoqun Zhao, Zijing Zhang, Zeyu Wang, Jinyuan Liu) at The University of Sydney as part of their capstone unit using the students-as-partners approach and supervised by University of Sydney ICT staff (Daniel Burn, Erica Bista).
+    
+    Licence: OurFlora is offered under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.')
   end
 end
