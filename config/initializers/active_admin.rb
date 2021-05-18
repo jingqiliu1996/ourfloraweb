aee5479e3fbe1338a5db9cd2ef01b5a94f951592
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-18 11:15:21 +0800

    Update active_admin.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = config.site_title = proc { I18n.t("admin.#{species_controller}.title") }
+  config.site_title = proc { I18n.t("admin.#{species_controller}.title") }
 #species_controller
   # Set the multipart limit so large forms don't crash
   Rack::Utils.multipart_part_limit = 0
