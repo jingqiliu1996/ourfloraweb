8d8fd5ef5888c31a5dc3aa65fdc4fae2c317e962
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-29 16:12:23 +0800

    Update family.rb

--- app/admin/family.rb
+++ app/admin/family.rb
@@ -1,13 +1,12 @@
 ActiveAdmin.register Family do
   permit_params :name, :phylogeny
   active_admin_import validate: true,
-  active_admin_import on_duplicate_key_ignore: true,
-    after_import:  ->(importer){
-      Family.transaction do
-        Family.connection.execute("DELETE FROM families WHERE name IN (SELECT name FROM families GROUP BY name HAVING COUNT(name)>1)")
-        
-      end
-  }
+  active_admin_import on_duplicate_key_ignore: true
+  #   after_import:  ->(importer){
+  #     Family.transaction do
+  #       Family.connection.execute("DELETE FROM families WHERE name IN (SELECT name FROM families GROUP BY name HAVING COUNT(name)>1)")
+  #     end
+  # }
 
   controller do
     def index
