723b26738b96a74a5f7472fa50f96a549a5ec282
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-05-30 16:31:14 +0800

    Update 20150204002833_add_genus_species_to_image.rb

--- db/migrate/20150204002833_add_genus_species_to_image.rb
+++ db/migrate/20150204002833_add_genus_species_to_image.rb
@@ -1,4 +1,4 @@
-class AddgenusspeciesToImage < ActiveRecord::Migration
+class AddGenusSpeciesToImage < ActiveRecord::Migration
   def change
     add_column :images, :genusspecies, :string
     Image.all.each do |image|
