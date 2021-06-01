d5e29b8c4d788acd1392a2069572566d1cbac499
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-06-01 09:15:58 +0800

    Update species.rb

--- app/admin/species.rb
+++ app/admin/species.rb
@@ -6,6 +6,7 @@ ActiveAdmin.register Species do
     Species.transaction do
       Species.connection.execute("update species inner join families on species.familyname=families.name set species.family_id=families.id")
       Species.connection.execute("insert into species_locations (species_id,lat,lon) select id,lat,lon from species")
+      Species.connection.execute("update species set slug=replace('slug',' ','_')")
       end
     }
 
