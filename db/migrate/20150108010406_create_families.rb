242e7d012f73c8e1ae65d970d67686bce5d0eec5
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-06-01 10:09:19 +0800

    current timestamp

--- db/migrate/20150108010406_create_families.rb
+++ db/migrate/20150108010406_create_families.rb
@@ -2,7 +2,7 @@ class CreateFamilies < ActiveRecord::Migration
   def change
     create_table :families do |t|
       t.string :name
-      t.timestamps default:Time.now, null: false
+      t.timestamps default:CURRENT_TIMESTAMP, null: false
     end
   end
 end
