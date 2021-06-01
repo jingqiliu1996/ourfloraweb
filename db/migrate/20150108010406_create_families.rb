ff28641ca403f360ef6dd7794a859ac2b0a8aefa
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-06-01 10:16:53 +0800

    1

--- db/migrate/20150108010406_create_families.rb
+++ db/migrate/20150108010406_create_families.rb
@@ -2,7 +2,7 @@ class CreateFamilies < ActiveRecord::Migration
   def change
     create_table :families do |t|
       t.string :name
-      t.timestamps default:Date.now, null: false
+      t.timestamps default:Date.now(), null: false
     end
   end
 end
